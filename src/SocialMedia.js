3d794d681e8b40ed5edfa0b6f17de5596ffb5627
hqur24 <78306792+hqur24@users.noreply.github.com>
2022-03-25 14:55:27 +0000

    extra changes, scrollbar + new location api key

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -21,7 +21,8 @@ const SocialMedia = ({ responseObj, mainCity }) => {
             "headers": {
                 'X-RapidAPI-Host': 'InstagramdimashirokovV1.p.rapidapi.com',
                 // 'X-RapidAPI-Key': '0a1494a602msh705260b0e4c166dp1cb901jsn36fcf81fb794'
-                'X-RapidAPI-Key': '8ea10601demshe52e0c1fc900b69p10293ejsnde41c0274d78'
+                // 'X-RapidAPI-Key': '8ea10601demshe52e0c1fc900b69p10293ejsnde41c0274d78'
+                'X-RapidAPI-Key': 'e462b31e40msh28d7cbc8c38f571p14467ejsne6fc086f6a66'
 
             }
         })
@@ -60,16 +61,15 @@ const SocialMedia = ({ responseObj, mainCity }) => {
     return (
             <div className='SocialMedia'>
                 <button className="instagramBtn" onClick={getPosts} >Check Instagram</button>
-                {/* <p>Here is whats popping on the gram for {responseObj.list[mainCity].name}</p> */}
                 <p className="postsIntroText" style = {{width: "352px"}}>Top posts from {responseObj.list[mainCity].name}!</p>
                 {gettem === true ?
                     data.map(post => (                
                     <div className='igTopPosts'>
 
                             <ol key={post["node"]["id"]}>
-                                <p className="usernameText">Username: {post["node"]["__typename"]}</p>
+                                {/* <p className="usernameText">Username: {post["node"]["__typename"]}</p> */}
                                 {/* cros error when uploading images */}
-                                <img id={post["node"]["id"]} src={post["node"]["thumbnail_src"]} width="100" height="100" />
+                                <img id={post["node"]["id"]} src={post["node"]["thumbnail_src"]} width="180" height="180" />
                                 {/* <p>{post["node"]["accessibility_caption"]}</p> */}
                             </ol>            </div>
 
